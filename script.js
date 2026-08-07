1332a9e4e9c140f37ff89580298980f0d2dba9d1
Monkemountainstudios <magnus@energiretur.se>
2026-08-07 23:12:58 +0200

    damn comma

--- script.js
+++ script.js
@@ -137,7 +137,7 @@ const scales = {
     lydian:     [0, 2, 4, 6, 7, 9, 11],
     mixolydian: [0, 2, 4, 5, 7, 9, 10],
 	pentatonic: [0, 2, 4, 7, 9],
-	ragatodi:	[0, 1, 3, 6, 7, 9, 10, 11]
+	ragatodi:	[0, 1, 3, 6, 7, 9, 10, 11],
 	wholetone:  [0, 2, 4, 6, 8, 10]
 };
 let currentScale = scales.minor;
